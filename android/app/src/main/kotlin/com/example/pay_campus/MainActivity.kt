1bf46ddd70dc67bb435a8abef43aed93c9971fb0
Diishhaa <dishahpati@gmail.com>
2026-07-08 18:07:50 +0530

    Initialize Flutter platform runner directories for mobile development

---
+++ android/app/src/main/kotlin/com/example/pay_campus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pay_campus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/paycampus/pay_campus/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.paycampus.pay_campus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
